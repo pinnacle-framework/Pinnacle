677891a776ecfcfdc551f7a99154d1eb729dbf5d
pinnacle-framework <arvidssone672@gmail.com>
2023-06-12 09:54:45 +0200

    Add misc.compat.cache from functools.cache
    Former-commit-id: 68497f54ddcf5d983279e18d7594c5ea315ea6db

--- superduperdb/apis/openai/wrapper.py
+++ superduperdb/apis/openai/wrapper.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 import os
 
 from openai import ChatCompletion as _ChatCompletion
@@ -8,11 +7,12 @@ from openai.error import Timeout, RateLimitError, TryAgain, ServiceUnavailableEr
 
 from pinnacledb.apis.utils import DoRetry
 from pinnacledb.core.model import Model
+from pinnacledb.misc.compat import cache
 
 do_retry = DoRetry((Timeout, RateLimitError, TryAgain, ServiceUnavailableError))
 
 
-@lru_cache(maxsize=None)
+@cache
 def _available_models():
     return tuple([r['id'] for r in OpenAIModel.list()['data']])
 
