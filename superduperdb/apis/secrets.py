531bd829e2a2feebeee8adeaba2550c3bc13b426
pinnacle-framework <arvidssone672@gmail.com>
2023-06-15 11:01:34 +0200

    Remove unused superduperdb/apis/secrets.py
    Former-commit-id: 09a33da5bd1da76495d5ab48a42541b5934845af

--- superduperdb/apis/secrets.py
+++
@@ -1,7 +0,0 @@
-import importlib
-from pinnacledb import CFG
-
-
-def client_init():
-    for provider in CFG.apis.providers:
-        importlib.import_module(f'pinnacledb.apis.{provider}.init').init_fn()
