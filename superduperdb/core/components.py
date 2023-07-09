7878f4e26cdab20d781e10757b1c0501e9eaf0d4
pinnacle-framework <arvidssone672@gmail.com>
2023-07-09 16:06:11 -0400

    Refactor signatures and simplified serialization
    Former-commit-id: b7a3c755487b48513d2c088d9813a54c73e162c4

--- superduperdb/core/components.py
+++ superduperdb/core/components.py
@@ -1,7 +1,6 @@
-from pinnacledb.core.fit import Fit
 from pinnacledb.core.metric import Metric
 from pinnacledb.core.model import Model
-from pinnacledb.core.model import TrainingConfiguration
+from pinnacledb.core.model import _TrainingConfiguration
 from pinnacledb.core.encoder import Encoder
 from pinnacledb.core.vector_index import VectorIndex
 from pinnacledb.core.watcher import Watcher
@@ -9,9 +8,8 @@ from pinnacledb.core.watcher import Watcher
 components = {
     'watcher': Watcher,
     'model': Model,
-    'fit': Fit,
     'metric': Metric,
-    'training_configuration': TrainingConfiguration,
+    'training_configuration': _TrainingConfiguration,
     'type': Encoder,
     'vector_index': VectorIndex,
 }
