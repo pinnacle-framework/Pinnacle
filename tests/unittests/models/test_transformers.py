da08a16db2d853e1d6b145665f944d10a3782211
pinnacle-framework <arvidssone672@gmail.com>
2023-07-21 11:02:57 +0200

    Create a test directory in a tdir
    Former-commit-id: 442d8465314315c02216b21d9a99d31ae30e66b9

--- tests/unittests/models/test_transformers.py
+++ tests/unittests/models/test_transformers.py
@@ -1,4 +1,5 @@
 import pytest
+import tdir
 
 from pinnacledb.core.documents import Document as D
 from pinnacledb.datalayer.mongodb.query import Collection
@@ -41,6 +42,7 @@ def test_transformer(trainer):
     pass
 
 
+@tdir
 def test_tranformers_trainer(trainer, random_data):
     trainer, tokenizer = trainer
 
