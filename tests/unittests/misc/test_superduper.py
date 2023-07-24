7d90fa83cd4056c76bd3a0e7b6a71338c825e4ee
pinnacle-framework <arvidssone672@gmail.com>
2023-07-25 02:06:22 +0530

    Fix a minor bug in superduper typer
    Former-commit-id: 2421c92f6ad8bec7cf42a37f0afa6bb1bf396e7e

---
+++ tests/unittests/misc/test_superduper.py
@@ -0,0 +1,43 @@
+import pytest
+
+from pinnacledb import pinnacle
+from pinnacledb.misc.pinnacle import MongoDbTyper, SklearnTyper, TorchTyper
+
+
+def test_mongodb_typer(test_db):
+    assert MongoDbTyper.accept(test_db.db) is True
+
+
+def test_sklearn_typer():
+    from sklearn.linear_model import LinearRegression
+
+    assert SklearnTyper.accept(LinearRegression()) is True
+
+
+def test_torch_typer():
+    import torch
+
+    assert TorchTyper.accept(torch.nn.Linear(1, 1)) is True
+
+
+def test_pinnacle_db(test_db):
+    db = pinnacle(test_db.db)
+    assert db.db == test_db.db
+
+
+def test_pinnacle_model():
+    from sklearn.linear_model import LinearRegression
+    import torch
+
+    model = pinnacle(torch.nn.Linear(1, 1))
+    assert isinstance(model.object.artifact, torch.nn.modules.linear.Linear)
+    model = pinnacle(LinearRegression())
+    assert isinstance(model.object.artifact, LinearRegression)
+
+
+def test_pinnacle_raise():
+    with pytest.raises(NotImplementedError):
+        pinnacle(1)
+
+    with pytest.raises(NotImplementedError):
+        pinnacle("string")
