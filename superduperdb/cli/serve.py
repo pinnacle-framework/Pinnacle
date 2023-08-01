10bc67b91475a59b7a8268fe40167d2acd55adea
pinnacle-framework <arvidssone672@gmail.com>
2023-08-01 15:00:25 +0200

    Get rid of import spam on startup
    Former-commit-id: 27520ca6c16fc62eb97ecd5555f677c484ef3f86

--- superduperdb/cli/serve.py
+++ superduperdb/cli/serve.py
@@ -1,29 +1,33 @@
 import typing as t
 
-from pinnacledb import CFG
-from pinnacledb.db.base.build import build_datalayer
-from pinnacledb.db.base.cdc import DatabaseWatcher
-from pinnacledb.db.mongodb.query import Collection
-from pinnacledb.server.dask_client import dask_client
-from pinnacledb.server.server import serve as _serve
+import pinnacledb as s
 
 from . import command
 
 
 @command(help='Start server')
 def serve():
+    from pinnacledb.db.base.build import build_datalayer
+    from pinnacledb.server.server import serve
+
     db = build_datalayer()
-    _serve(db)
+    serve(db)
 
 
 @command(help='Start local cluster: server, dask and change data capture')
 def local_cluster(on: t.List[str] = []):
+    from pinnacledb.db.base.build import build_datalayer
+    from pinnacledb.db.base.cdc import DatabaseWatcher
+    from pinnacledb.db.mongodb.query import Collection
+    from pinnacledb.server.dask_client import dask_client
+    from pinnacledb.server.server import serve
+
     db = build_datalayer()
-    dask_client(CFG.dask, local=True)
+    dask_client(s.CFG.dask, local=True)
     for collection in on:
         w = DatabaseWatcher(
             db=db,
             on=Collection(name=collection),
         )
         w.watch()
-    _serve(db)
+    serve(db)
