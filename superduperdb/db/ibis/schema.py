408a5a930704b88b2ead6e932d673d6d178a43aa
pinnacle-framework <arvidssone672@gmail.com>
2023-08-25 10:47:55 +0200

    Add decoding to SuperDuperIbisCursor
    Former-commit-id: 995c4e83cad9e69356604e1055267274501b0337

--- superduperdb/db/ibis/schema.py
+++ superduperdb/db/ibis/schema.py
@@ -1,13 +1,14 @@
 import dataclasses as dc
+import typing as t
 
 import ibis
 
 from pinnacledb.container.schema import Schema
 from pinnacledb.container.encoder import Encoder
 
+@dc.dataclass
 class IbisSchema(Schema):
-    type_id: str = 'ibis.schema'
-    encoded_types = []
+    encoded_types: t.List = dc.field(default_factory=list)
 
     def map(self):
         if self.trivial:
