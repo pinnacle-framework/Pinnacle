c106b2b153e8b6408ae2a72d16b1f557627065e1
pinnacle-framework <arvidssone672@gmail.com>
2023-09-11 14:39:15 +0200

    Add apps to test_impall
    Former-commit-id: 4c69df49f265bec0bd1d0db7ce535fde624dbe45

--- test/unittest/test_impall.py
+++ test/unittest/test_impall.py
@@ -9,3 +9,4 @@ class ImpAllTest(impall.ImpAllTest):
     CLEAR_SYS_MODULES = False
     WARNINGS_ACTION = 'ignore'
     EXCLUDE = (torch is None) * ['pinnacledb/ext/torch/**']
+    PATHS = 'pinnacledb', 'test', 'apps/question-the-docs'
