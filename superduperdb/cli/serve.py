b94d5ec3485ffd79614d5b455dadb258604b1510
pinnacle-framework <arvidssone672@gmail.com>
2023-10-18 18:44:37 +0530

    Make cdc consumer singleton
    Former-commit-id: 59dc8d7e234656d6263bce7e4bdbcef25e7f768b

--- superduperdb/cli/serve.py
+++ superduperdb/cli/serve.py
@@ -17,7 +17,6 @@ def serve():
 @command(help='Start local cluster: server, dask and change data capture')
 def local_cluster(on: t.List[str] = []):
     from pinnacledb.db.base.build import build_datalayer
-    from pinnacledb.db.base.cdc import DatabaseListener
     from pinnacledb.db.mongodb.query import Collection
     from pinnacledb.server.dask_client import dask_client
     from pinnacledb.server.server import serve
@@ -28,9 +27,7 @@ def local_cluster(on: t.List[str] = []):
         local=True,
     )
     for collection in on:
-        w = DatabaseListener(
-            db=db,
+        db.cdc.start(
             on=Collection(identifier=collection),
         )
-        w.listen()
     serve(db)
