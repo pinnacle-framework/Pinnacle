190472441f079d0b997e14e0eaf39956348a8e27
pinnacle-framework <arvidssone672@gmail.com>
2023-12-21 20:56:11 +0530

    fix 'Edit this page' URLs (#1581)

--- docs/hr/docusaurus.config.js
+++ docs/hr/docusaurus.config.js
@@ -181,6 +181,9 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
+
+            'https://github.com/SuperDuperDB/pinnacledb/blob/main/docs/hr',
+
             'https://github.com/SuperDuperDB/pinnacledb/blob/main/docs',
         },
         blog: {
@@ -188,7 +191,7 @@ const config = {
           // Please change this to your repo
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/SuperDuperDB/pinnacledb/tree/main/docs',
+            'https://github.com/SuperDuperDB/pinnacledb/blob/main/docs/hr',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
