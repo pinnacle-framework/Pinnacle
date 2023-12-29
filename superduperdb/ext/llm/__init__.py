e71e7990e24ea65f155d0a6e180d98346d461f67
pinnacle-framework <arvidssone672@gmail.com>
2023-12-29 23:28:06 +0800

    Organize the import of llm class

---
+++ superduperdb/ext/llm/__init__.py
@@ -0,0 +1,9 @@
+from pinnacledb.ext.llm.openai import OpenAI
+from pinnacledb.ext.llm.vllm import VllmAPI, VllmModel, VllmOpenAI
+
+__all__ = [
+    "OpenAI",
+    "VllmAPI",
+    "VllmModel",
+    "VllmOpenAI",
+]
