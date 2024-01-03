bbae3cf6a3b8d8514a9cddc923a68192fc69e78c
pinnacle-framework <arvidssone672@gmail.com>
2024-01-03 23:36:53 +0800

    Optimize llm related code
    - Support dialog format messages
    - Delete the model class in vllm openai format
    - Use multi-threading instead of async function for concurrent API requests

--- superduperdb/ext/llm/__init__.py
+++ superduperdb/ext/llm/__init__.py
@@ -1,9 +1,12 @@
+from pinnacledb.ext.llm.base import BaseLLMAPI, BaseLLMModel, BaseOpenAI
 from pinnacledb.ext.llm.openai import OpenAI
-from pinnacledb.ext.llm.vllm import VllmAPI, VllmModel, VllmOpenAI
+from pinnacledb.ext.llm.vllm import VllmAPI, VllmModel
 
 __all__ = [
+    "BaseOpenAI",
+    "BaseLLMModel",
+    "BaseLLMAPI",
     "OpenAI",
     "VllmAPI",
     "VllmModel",
-    "VllmOpenAI",
 ]
