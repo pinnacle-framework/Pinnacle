a13526d87383a61847a256eade480ffcbb64978a
pinnacle-framework <arvidssone672@gmail.com>
2024-05-16 15:02:54 +0300

    fix credentials and dependencies
    Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

--- deploy/databases/oracle/example.py
+++ deploy/databases/oracle/example.py
@@ -1,8 +1,23 @@
-from pinnacledb import pinnacle
+import ibis
 
 user = 'pinnacle'
 password = 'pinnacle'
 port = 1521
 host = 'localhost'
+database = 'test_db'
 
-db = pinnacle(f"oracle://{user}:{password}@{host}:{port}")
+# OK
+con = ibis.oracle.connect(
+    user = 'pinnacle',
+    password = 'pinnacle',
+    port = 1521,
+    host = 'localhost',
+    database = 'test_db',
+)
+
+# ERROR AttributeError: 'str' object has no attribute 'username'
+#con = ibis.connect(f"oracle://{user}:{password}@{host}:{port}/{database}")
+
+# ERROR ModuleNotFoundError: No module named 'ibis.backends.base'
+# from pinnacledb import pinnacle
+#db = pinnacle(f"oracle://{user}:{password}@{host}:{port}/{database}")
