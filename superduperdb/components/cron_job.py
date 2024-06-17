605853c6c042c52140e6f7a4a244fffc1898e24b
pinnacle-framework <arvidssone672@gmail.com>
2024-06-17 09:05:39 +0200

    Simple cronjobs

--- superduperdb/components/cron_job.py
+++ superduperdb/components/cron_job.py
@@ -1,4 +1,11 @@
-from pinnacledb import Component
+from pinnacledb import Component, CFG, logging
+from pinnacledb.base.datalayer import Datalayer
+from pinnacledb.components.component import ensure_initialized
+from pinnacledb.components.datatype import dill_serializer
+
+import typing as t
+
+from pinnacledb.misc.server import request_server
 
 
 class CronJob(Component):
@@ -9,8 +16,41 @@ class CronJob(Component):
     :param schedule: Schedule in cron format.
     """
 
+    type_id: t.ClassVar[str] = 'cronjob'
     schedule: str = '0 0 * * *'
 
+    def post_create(self, db: Datalayer) -> None:
+        super().post_create(db)
+        if CFG.cluster.crontab.uri is not None:
+            request_server(
+                service='crontab',
+                endpoint='add',
+                args={'identifier': self.identifier},
+                type='post',
+            )
+        else:
+            logging.warn('No crontab service found - cron-job will not schedule')
+
+    @ensure_initialized
     def run(self):
         """Run the job."""
         raise NotImplementedError
+
+
+class FunctionCronJob(CronJob):
+    """
+    Run a function on a schedule.
+
+    :param function: Callable to run
+    """
+    _artifacts = (
+        ('function', dill_serializer),
+    )
+
+    function: t.Callable
+
+    @ensure_initialized
+    def run(self):
+        self.function(self.db)
+
+
