4ce61032aa1986d96a1c4bf5d998f10d72394d92
pinnacle-framework <arvidssone672@gmail.com>
2024-08-12 15:26:32 +0600

    Add robots txt and edit url in docusauraus (#2381)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -194,7 +194,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/pinnacle-io/pinnacle/blob/main/docs/hr',
+            'https://github.com/pinnacle-io/pinnacle/edit/main/docs/',
         },
         blog: false,
         theme: {
