dbd0709683ef9196974b8cb011e0c682022532fe
pinnacle-framework <arvidssone672@gmail.com>
2024-08-14 16:52:15 +0800

    Optimize the logic for file saving and retrieval in the artifact_store.

--- test/conftest.py
+++ test/conftest.py
@@ -2,15 +2,15 @@ import os
 from pathlib import Path
 from typing import Iterator
 
+import pytest
+
 pinnacle_CONFIG = os.environ.get("pinnacle_CONFIG", "test/configs/default.yaml")
 
 os.environ["pinnacle_CONFIG"] = pinnacle_CONFIG
 
-import pytest
 
 from pinnacle import pinnacle
 from pinnacle.base.datalayer import Datalayer
-from pinnacle.base.enums import DBType
 
 
 @pytest.fixture
@@ -18,14 +18,7 @@ def db() -> Iterator[Datalayer]:
     db = pinnacle()
 
     yield db
-    db_type = db.databackend.db_type
-    if db_type == DBType.MONGODB:
-        db.drop(force=True, data=True)
-    elif db_type == DBType.SQL:
-        db.artifact_store.drop(force=True)
-        tables = db.databackend.conn.list_tables()
-        for table in tables:
-            db.databackend.conn.drop_table(table, force=True)
+    db.drop(force=True, data=True)
 
 
 @pytest.fixture(scope='session')
