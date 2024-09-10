c45b31c74b64cd88c34579af8ca618b9f02b9085
pinnacle-framework <arvidssone672@gmail.com>
2024-09-10 06:12:00 +0530

    feat: Qdrant vector search support (#2428)

--- pinnacle/backends/base/backends.py
+++ pinnacle/backends/base/backends.py
@@ -1,9 +1,11 @@
 from pinnacle.vector_search.atlas import MongoAtlasVectorSearcher
 from pinnacle.vector_search.in_memory import InMemoryVectorSearcher
 from pinnacle.vector_search.lance import LanceVectorSearcher
+from pinnacle.vector_search.qdrant import QdrantVectorSearcher
 
 vector_searcher_implementations = {
     "lance": LanceVectorSearcher,
     "in_memory": InMemoryVectorSearcher,
     "mongodb+srv": MongoAtlasVectorSearcher,
+    "qdrant": QdrantVectorSearcher,
 }
