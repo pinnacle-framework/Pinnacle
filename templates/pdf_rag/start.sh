b3ad6e7c49ecce4b5af46aa3cdb4c021f328819c
pinnacle-framework <arvidssone672@gmail.com>
2025-06-14 22:13:15 +0200

    Remove templates

--- templates/pdf_rag/start.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p data/artifacts
-pinnacle bootstrap ./templates/pdf_rag
-python3 templates/pdf_rag/add_data.py data/$1
-pinnacle apply pdf-rag --variables '{"table_name": "'$1'", "embedding_model": "'$2'", "llm_model": "'$3'"}'
